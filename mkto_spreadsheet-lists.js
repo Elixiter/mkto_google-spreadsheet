cdc9c1b38f4adbbbeac1198c415fc8ebd0286d8c
Kyle Halstvedt <khalstvedt@gmail.com>
2014-08-20 17:13:09 -0600

    comment clean-up

--- mkto_spreadsheet-lists.js
+++ mkto_spreadsheet-lists.js
@@ -1,10 +1,14 @@
+// ==================================================
+// BEGIN frontmatter
+//
+//
 // Author information
 // --------------------
 // Kyle Halstvedt
 // Elixiter, Inc.
 // kyle@elixiter.com
 // --------------------
-
+//
 // Setup (*IMPORTANT*)
 // --------------------
 // 1. From within a new Google Spreadsheet file,
@@ -21,7 +25,7 @@
 //    Marketo's documentation at
 //    http://developers.marketo.com/documentation/rest/
 // --------------------
-
+//
 // Usage
 // --------------------
 // 1. From a Spreadsheet file with the script
@@ -32,29 +36,35 @@
 // 3. Each list has an "Insert" button. Click it to insert
 //    the members of the list.
 // --------------------
-
+//
 // Todo / Caveats
 // --------------------
 // 1. Paginated API; currently only retrieves
 //    the first 100 results from a list.
 // --------------------
+//
+//
+// END: frontmatter
+// ==================================================
 
-// configuration: YOU MUST REPLACE THESE VALUES,
-//   keeping the quotation marks intact.
+// Configuration
+// -------------
+// YOU MUST REPLACE THESE VALUES, keeping the quotation marks intact.
 var restEndpoint = 'REPLACE_ME'; // Marketo REST API endpoint
 var identityUrl = 'REPLACE_ME'; // Marketo REST API identity service
 var consumerKey = 'REPLACE_ME'; // Marketo REST API client ID
 var consumerSecret = 'REPLACE_ME'; // Marketo REST API client secret
-// END configuration
+// END: configuration
 
 // when the document is opened, create the top menu
 function onOpen() {
   var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
   var ui = SpreadsheetApp.getUi();
-  var menu = ui.createMenu('Marketo Import')
-    .addItem('Initialize sidebar...', 'fetchLists')
+  var menu = ui.createMenu('Marketo Import');
+  menu.addItem('Initialize sidebar...', 'fetchLists');
   menu.addToUi();
 }
+// END: onOpen()
 
 // fetch list names from the REST API
 function fetchLists() {
@@ -69,9 +79,10 @@ function fetchLists() {
       name : parsedResponse.result[n].name
     });
   }
-  // pass lists to the sidebar
+  // create a sidebar with the array of list ID's/names
   createSidebar(listsArray);
 }
+// END: fetchLists()
 
 // create sidebar to display list names and ID's
 // also includes 'insert' button to copy the list to the current spreadsheet
@@ -85,30 +96,33 @@ function createSidebar(listsArray) {
   for (var l in listsArray) {
     var horizontal = app.createHorizontalPanel();
     var button = app.createButton('Insert').setId(listsArray[l].id); // set button ID to MKTO list ID
-    var handler = app.createServerHandler('buttonHandler');
-    horizontal.setVerticalAlignment(UiApp.VerticalAlignment.MIDDLE).setSpacing(10); // set format options
-    horizontal.add(button);
-    horizontal.add(app.createLabel(listsArray[l].id));
-    horizontal.add(app.createLabel(listsArray[l].name));
-    button.addClickHandler(handler);
+    var idLabel = app.createLabel(listsArray[l].id);
+    var nameLabel = app.createLabel(listsArray[l].name);
+    var handler = app.createServerHandler('buttonHandler'); // specify handler function
+    button.addClickHandler(handler); // attach handler to button click event
+    horizontal.setVerticalAlignment(UiApp.VerticalAlignment.MIDDLE).setSpacing(10); // set panel format options
+    horizontal.add(button).add(idLabel).add(nameLabel); // add button and labels all at once
+//    horizontal.add(idLabel);
+//    horizontal.add(nameLabel);
     vertical.add(horizontal);
   }
+  // END: for (var l in listsArray)
   scroll.add(vertical);
   app.add(scroll);
-  SpreadsheetApp.getUi().showSidebar(app);
+  SpreadsheetApp.getUi().showSidebar(app); // finally, display the sidebar
 }
+// END: createSidebar()
 
 // handle click events from sidebar buttons
-// the button's ID is set to the MKTO list ID, contained
-//   in eventInfo.parameter.source
 function buttonHandler(eventInfo) {
-  var list = fetchList(eventInfo.parameter.source)
+  var list = fetchList(eventInfo.parameter.source); // source is ID for button element
   insertList(list);
 }
+// END: buttonHandler()
 
 // retrieve list by ID from MKTO REST API
 // returns an array of objects with keys:
-// id, email, firstName, lastName
+//   id, email, firstName, lastName
 function fetchList(id) {
   var listArray = [];
   var bearerToken = JSON.parse(UrlFetchApp.fetch(identityUrl + 'oauth/token?grant_type=client_credentials&client_id=' + consumerKey + '&client_secret=' + consumerSecret).getContentText()).access_token;
@@ -125,6 +139,7 @@ function fetchList(id) {
   }
   return listArray;
 }
+// END: fetchList()
 
 // insert the contents of the "list" array
 //   into the currently-active sheet
@@ -136,3 +151,4 @@ function insertList(list) {
     sheet.appendRow([row.id, row.email, row.firstName, row.lastName]);
   }
 }
+// END: insertList()
